dbef9ef67f2e80515ea3917505bb31d0403345f6
EmmaVgn <emma.vigneron@outlook.fr>
2024-09-16 15:48:12 +0200

    add popup homepage

--- assets/app.js
+++ assets/app.js
@@ -42,4 +42,36 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
+//popup
+document.addEventListener('DOMContentLoaded', (event) => {
+    const contentLinks = document.querySelectorAll('.content-link');
+    const popups = document.querySelectorAll('.popup');
+    const closeButtons = document.querySelectorAll('.close');
+  
+    contentLinks.forEach(link => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent default anchor action
+        const popupId = link.getAttribute('data-popup');
+        const popup = document.getElementById(popupId);
+        if (popup) {
+          popup.style.display = 'block'; // Show the popup
+        }
+      });
+    });
+  
+    closeButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const popup = button.closest('.popup');
+        if (popup) {
+          popup.style.display = 'none'; // Hide the popup
+        }
+      });
+    });
+  
+    window.addEventListener('click', (e) => {
+      if (e.target.classList.contains('popup')) {
+        e.target.style.display = 'none'; // Hide the popup when clicking outside of it
+      }
+    });
+  });
+  
