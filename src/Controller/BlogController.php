049a2f3176721c7945b4b5b97610db4a109240d0
EmmaVgn <emma.vigneron@outlook.fr>
2024-09-19 16:41:54 +0200

    add blog entity

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/actualites', name: 'blog_index')]
-    public function index(): Response
+    #[Route('/blog', name: 'blog_list')]
+    public function list(ArticleRepository $articleRepository): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        $articles = $articleRepository->findAll();
+        return $this->render('blog/list.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/blog/{id}', name: 'blog_show')]
+    public function show(int $id, ArticleRepository $articleRepository): Response
+    {
+        $article = $articleRepository->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('Article not found');
+        }
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
         ]);
     }
 }
