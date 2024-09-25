a1dc38b84bb0bd018a1210328ef23f567808996f
EmmaVgn <emma.vigneron@outlook.fr>
2024-09-25 15:15:19 +0200

    add css, js et update blog page for vich

--- assets/app.js
+++ assets/app.js
@@ -120,6 +120,19 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// blog page
+document.addEventListener("DOMContentLoaded", function() {
+  const articles = document.querySelectorAll('.card');
 
+  articles.forEach((article, index) => {
+      article.style.opacity = 0;
+      article.style.transform = 'translateY(20px)';
+      setTimeout(() => {
+          article.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+          article.style.opacity = 1;
+          article.style.transform = 'translateY(0)';
+      }, index * 100);
+  });
+});
 
 
