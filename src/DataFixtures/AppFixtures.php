3b870ddd43ae73ba5dc590ba7ca7db5c1af5ba2c
EmmaVgn <emma.vigneron@outlook.fr>
2024-09-25 16:40:10 +0200

    add user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,48 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker;
+use App\Entity\User;
+
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger
+    ){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail('admin@gmail.fr');
+        $admin->setLastname('Vigneron');
+        $admin->setFirstname('Emma');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'admin')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($usr = 1; $usr <= 15; $usr++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($user, 'secret')
+            );
+          
+           
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
