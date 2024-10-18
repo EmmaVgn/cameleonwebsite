7237e597bc90316414cefd76467f2e82674cb7e1
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-18 14:07:39 +0200

    Fix bug modal

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,63 +11,67 @@ class HomeController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-    $project = [
-        [
-            'id' => 1,
-            'title' => 'Garage Thomas',
-            'short_description' => 'Création d\'un site vitrine pour un garage automobile.',
-            'description' => 'Réalisation d\'un site vitrine pour un garage automobile dans le cadre d\un projet étudiant. Présentation des services et des véhicules d\'occasion en vente.',
-            'image' =>  '/img/project a/Image-7.png',
-            'images' => [
-                '/img/project a/Image-6.png',
-                '/img/project a/Image-10.png',
-                '/img/project a/Image-11.png',
-                '/img/project a/Image-3.png',
-            ],
-        ],
-        [
-            'id' => 2,
-            'title' => 'Aromelis',
-            'short_description' => 'Création d\'un site e-commerce pour une apicultrice incluant la charte graphique ',
-            'description' => 'Site e-commerce pour vente de miel, huiles essentielles, recharges d\hydrolats et produits de la ruche. ',
-            'image' => '/img/project b/Image-13.png',
-            'images' => [
-                '/img/project b/Image-14.png',
-                '/img/project b/Image-12.png',
-                '/img/project b/Image-15.png',
-                '/img/project b/Image-16.png',
-                '/img/project b/Image-17.png',
-            ],
-        ],
-        [
-            'id' => 3,
-            'title' => 'Application Météo',
-            'short_description' => 'Création d\'une application météo.',
-            'description' => 'Création d\'une application météo avec affichage des températures, des conditions météorologiques.',
-            'image' => '/img/project c/Image-18.png',
-            'images' => [
-                '/img/project c/Image-19.png',
-            ],
-        ],
-        [
-            'id' => 4,
-            'title' => 'Les maisons de Marie',
-            'short_description' => 'Création d\'un site pour un établissement touristique',
-            'description' => 'Création d\'un site pour un établissement touristique. Présentation des chambres avec possibilité de réservation sur le site, des activités et des tarifs.',
-            'image' => '/img/project d/Image-20.png',
-            'images' => [
-                '/img/project d/Image-22.png',
-                '/img/project d/Image-21.png',
-            ],
-        ],
-  
-    ];
+        $projects = $this->getProjects();
 
-    return $this->render('home/index.html.twig', [
-        'controller_name' => 'Homepage',
-        'project' => $project,
-    ]);
-}
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'Homepage',
+            'projects' => $projects, // Corrigé de 'project' à 'projects'
+        ]);
+    }
+
+    private function getProjects(): array 
+    {
+        return [
+            [ 
+                'id' => 1,
+                'title' => 'Garage Thomas',
+                'short_description' => 'Création d\'un site vitrine pour un garage automobile.',
+                'description' => 'Réalisation d\'un site vitrine pour un garage automobile dans le cadre d\'un projet étudiant. Présentation des services et des véhicules d\'occasion en vente.',
+                'image' =>  '/img/project a/Image-7.png',
+                'images' => [
+                    '/img/project a/Image-6.png',
+                    '/img/project a/Image-10.png',
+                    '/img/project a/Image-11.png',
+                    '/img/project a/Image-3.png',
+                ],
+            ],
+            [
+                'id' => 2,
+                'title' => 'Aromelis',
+                'short_description' => 'Création d\'un site e-commerce pour une apicultrice incluant la charte graphique',
+                'description' => 'Site e-commerce pour vente de miel, huiles essentielles, recharges d\'hydrolats et produits de la ruche.',
+                'image' => '/img/project b/Image-13.png',
+                'images' => [
+                    '/img/project b/Image-14.png',
+                    '/img/project b/Image-12.png',
+                    '/img/project b/Image-15.png',
+                    '/img/project b/Image-16.png',
+                    '/img/project b/Image-17.png',
+                ],
+            ],
+            [
+                'id' => 3,
+                'title' => 'Application Météo',
+                'short_description' => 'Création d\'une application météo.',
+                'description' => 'Création d\'une application météo avec affichage des températures, des conditions météorologiques.',
+                'image' => '/img/project c/Image-18.png',
+                'images' => [
+                    '/img/project c/Image-19.png',
+                ],
+            ],
+            [
+                'id' => 4,
+                'title' => 'Les maisons de Marie',
+                'short_description' => 'Création d\'un site pour un établissement touristique',
+                'description' => 'Création d\'un site pour un établissement touristique. Présentation des chambres avec possibilité de réservation sur le site, des activités et des tarifs.',
+                'image' => '/img/project d/Image-20.png',
+                'images' => [
+                    '/img/project d/Image-22.png',
+                    '/img/project d/Image-21.png',
+                ],
+            ],
+        ];
+    }
 
     #[Route('/mentions-legales', name: 'home_legal')]
     public function legal(): Response
@@ -86,6 +90,4 @@ class HomeController extends AbstractController
     {
         return $this->render('home/politique.html.twig');
     }
-
 }
-
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -8,39 +8,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    public function list(): Response
-    {
-        $projects = [
-            [
-                'id' => 1,
-                'title' => 'Projet A',
-                'short_description' => 'Description courte du projet A.',
-                'description' => 'Description détaillée du projet A.',
-                'image' => '/images/project-a.jpg',
-                'images' => [
-                    '/images/project-a-1.jpg',
-                    '/images/project-a-2.jpg',
-                    '/images/project-a-3.jpg',
-                ],
-            ],
-            [
-                'id' => 2,
-                'title' => 'Projet B',
-                'short_description' => 'Description courte du projet B.',
-                'description' => 'Description détaillée du projet B.',
-                'image' => '/images/project-b.jpg',
-                'images' => [
-                    '/images/project-b-1.jpg',
-                    '/images/project-b-2.jpg',
-                    '/images/project-b-3.jpg',
-                ],
-            ],
-            // Ajoute d'autres projets ici
-        ];
 
-        return $this->render('project/realisations.html.twig', [
-            'projects' => $projects,
-        ]);
-    }
+
 }
