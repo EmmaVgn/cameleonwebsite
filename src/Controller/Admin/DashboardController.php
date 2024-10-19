1a35461bca774434259b5b7fe7a709a43cbbb8ac
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-19 14:20:53 +0200

    annule tag

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Entity\Contact;
+use App\Entity\Tag;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -32,6 +33,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Blog', 'fas fa-microblog', Article::class);
+
         yield MenuItem::linkToCrud('Messages', 'fas fa-envelope', Contact::class);
         yield MenuItem::linkToRoute('Retour au site', 'fas fa-home', 'homepage');
     }
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -31,4 +31,3 @@ class BlogController extends AbstractController
         ]);
     }
 }
-
