5ffa8c49ade4c723851e548ab5a382451eeff743
EmmaVgn <emma.vigneron@outlook.fr>
2024-11-14 15:38:13 +0100

    ajout des cookies

--- assets/app.js
+++ assets/app.js
@@ -5,19 +5,43 @@ import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (
+        value || ""
+    ) + expires + "; path=/; SameSite=None; Secure";
+}
 
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
+window.addEventListener('load', function () {
+    console.log('Initialisation de CookieConsent');
+    if (window.cookieconsent) {
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#000"
+                },
+                "button": {
+                    "background": "#f1d600"
+                }
+            },
+            "theme": "classic",
+            "position": "bottom",
+            "content": {
+                "message": "Nous utilisons des cookies pour améliorer votre expérience sur notre site.",
+                "dismiss": "Accepter",
+                "link": "En savoir plus",
+                "href": "/politique-de-confidentialite"
+            },
+            onStatusChange: function (status) {
+                setCookie('cookieconsent_status', status, 365);
+            }
+        });
+    } else {
+        console.error('CookieConsent library not loaded.');
+    }
+});
