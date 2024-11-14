21f9da600d5b144d94398d2c11098afa8a9d3f28
EmmaVgn <emma.vigneron@outlook.fr>
2024-11-14 17:22:43 +0100

    ajout des mentions légales

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,8 @@ import './bootstrap.js';
 import './styles/app.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap';
+import './js/cookieconsent.min.js';
+import './styles/cookieconsent.min.css';
 
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
