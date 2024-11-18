096b67e9a04e6935879af2242eba11c6ebf30a17
EmmaVgn <emma.vigneron@outlook.fr>
2024-11-18 15:08:25 +0100

    modif pour optimiser le site

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,40 +1,38 @@
-//popup
 document.addEventListener('DOMContentLoaded', (event) => {
-    const contentLinks = document.querySelectorAll('.content-link');
-    const popups = document.querySelectorAll('.popup');
-    const closeButtons = document.querySelectorAll('.close');
-  
-    contentLinks.forEach(link => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault(); // Prevent default anchor action
-        const popupId = link.getAttribute('data-popup');
-        const popup = document.getElementById(popupId);
-        if (popup) {
-          popup.style.display = 'block'; // Show the popup
-        }
-      });
-    });
-  
-    closeButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        const popup = button.closest('.popup');
-        if (popup) {
-          popup.style.display = 'none'; // Hide the popup
-        }
-      });
-    });
-  
-    window.addEventListener('click', (e) => {
-      if (e.target.classList.contains('popup')) {
-        e.target.style.display = 'none'; // Hide the popup when clicking outside of it
+  const contentLinks = document.querySelectorAll('.content-link');
+  const popups = document.querySelectorAll('.popup');
+  const closeButtons = document.querySelectorAll('.close');
+
+  contentLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault(); // Empêche l'action par défaut (si c'était un lien)
+      const popupId = link.getAttribute('data-popup');
+      const popup = document.getElementById(popupId);
+      if (popup) {
+        popup.style.display = 'block'; // Affiche le popup
       }
     });
   });
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const popup = button.closest('.popup');
+      if (popup) {
+        popup.style.display = 'none'; // Masque le popup
+      }
+    });
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target.classList.contains('popup')) {
+      e.target.style.display = 'none'; // Masque le popup si on clique en dehors de celui-ci
+    }
+  });
+  
   const contactButtons = document.querySelectorAll('.contact-button-popup');
   contactButtons.forEach(button => {
-      button.addEventListener('click', function() {
-          window.location.href = '/contact'; 
-      });
-  }
-);
-  
+    button.addEventListener('click', function() {
+      window.location.href = '/contact'; 
+    });
+  });
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,28 +27,28 @@ class HomeController extends AbstractController
             'title' => 'Garage Thomas',
             'short_description' => 'Création d\'un site vitrine pour un garage automobile.',
             'link' => 'https://garagethomas.cameleon-solutions.fr/', // Lien vers le site
-            'image' =>  '/img/project a/Image-6.png',
+            'image' =>  '/img/project a/Image-6.webp',
         ],
         [
             'id' => 2,
             'title' => 'Aromelis',
             'short_description' => 'Site e-commerce pour une apicultrice.',
             'link' => 'https://aromielis.cameleon-solutions.fr/', // Lien vers le site
-            'image' => '/img/project b/Image-13.png',
+            'image' => '/img/project b/Image-13.webp',
         ],
         [
             'id' => 3,
             'title' => 'Application Météo',
             'short_description' => 'Application météo interactive.',
               'link' => '#',
-            'image' => '/img/project c/Image-18.png',
+            'image' => '/img/project c/Image-18.webp',
         ],
         [
             'id' => 4,
             'title' => 'Les maisons de Marie',
             'short_description' => 'Site de réservation pour un établissement touristique.',
             'link' => 'https://lesmaisonsdemarie.cameleon-solutions.fr/', // Lien vers le site
-            'image' => '/img/project d/Image-20.png',
+            'image' => '/img/project d/Image-20.webp',
         ],
     ];
 }
